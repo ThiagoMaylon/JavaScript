232c2bc5ed07c67b2dcb1f8008fb1007c588955c
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-08 05:32:36 -0300

    aula de condições pt 1

---
+++ aula011/ex008.js
@@ -0,0 +1,5 @@
+var idade = 17 ;
+
+if(idade >= 18){
+    console.log('autorizado')
+}
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = "EUA";
+
+if(pais == "Brasil"){
+    console.log("vc é brasileiro")
+}else{
+    console.log("vc é estrangeiro")
+}
