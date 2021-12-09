964d6f27ad937e16c6274729c94a6c44fae3d591
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-09 06:09:33 -0300

    exercicio resolvido

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,4 +1,19 @@
-var agora = new Date()
-var hora = agora.getHours
-var msg = document.querySelector('div#msg')
-var foto = document.querySelector('div#foto')
+function carregar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+
+    msg.innerHTML = `agora são ${hora} horas`
+
+    if(hora >= 4 && hora < 12){
+        img.src = 'imagens/dia.png'
+        window.document.body.style.backgroundColor = '#F2E638'
+    }else if(hora >= 12 && hora < 18){
+        img.src = 'imagens/tarde.png'
+        window.document.body.style.backgroundColor = '#F2AA80'
+    }else{
+        img.src = 'imagens/noite.png'
+        window.document.body.style.backgroundColor = '#585859'
+    }
+}
