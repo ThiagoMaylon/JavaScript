0d60827f0c47349c6013d6b54a789b1b50dbd168
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-14 06:34:08 -0300

    resolvendo o desafio 1

--- desafios/d001/script.js
+++ desafios/d001/script.js
@@ -1,13 +1,9 @@
-const button = document.querySelector('input');
-button.addEventListener('click', clicar);
-function clicar() {
-    if(button.value === 'Botão 1'){
-        window.alert("vc clicou o botao 1")
-
-    }else if(button.value === 'Botão 2'){
-        window.alert("vc clicou o botao 2")
-
-    }else if(button.value === 'Botão 3'){
-        window.alert("vc clicou o botao 3")
-    }
+function clickb1(){
+    window.alert('Botão 1')
+}
+function clickb2(){
+    window.alert('Botão 2')
+}
+function clickb3(){
+    window.alert('Botão 3')
 }
