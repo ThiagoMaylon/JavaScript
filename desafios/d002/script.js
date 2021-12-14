c109a6339f6b42eb9dbd7f09945ff67b242c1396
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-14 13:01:04 -0300

    completando o desafio 2

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function iniciar(){
+    var nome = window.prompt('Qual seu nome?')
+    var idade = window.prompt(`olá ${nome}! tem quantos anos?`)
+    window.alert(`acabei de conhecer ${nome}, que tem ${idade} anos`)
+}
