892e19d3dd9e837c9c4617d60b087f563158e477
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-15 13:24:03 -0300

    corrigindo erros do desafio

--- exercicio05/script.js
+++ exercicio05/script.js
@@ -1,23 +1,11 @@
-let res = document.getElementById('resultado')
-let valores = []
-let numero = Number(numtxt.value)
-let numtxt = document.getElementById('numtxt')
-
-
-function nalista(n, v){
-    if(v.indexOf(n) != -1){
-        return true
-    }else{
-        return false
-    }
-}
-
-
 function adicionar(){
-
+    var numtxt = document.getElementById('numtxt')
+    var res = document.getElementById('resultado')
+    var valores = []
+    var numero = Number(numtxt.value)
     let sel = document.getElementById('selnum')
 
-    if(numero >= 1 && numero <= 100 && nalista(numero, valor) == true){
+    if(numero >= 1 && numero <= 100){
         let item = document.createElement('option')
         item.text = `valor adiocionado = ${numero}`;
         sel.appendChild(item)  
