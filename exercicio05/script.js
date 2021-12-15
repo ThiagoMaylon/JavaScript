d020eb885fdfb1daaa88b3b3584b9c2ee1d7f6fc
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2021-12-15 20:43:07 -0300

    completando o exerccio 5

--- exercicio05/script.js
+++ exercicio05/script.js
@@ -1,18 +1,62 @@
-function adicionar(){
-    var numtxt = document.getElementById('numtxt')
-    var res = document.getElementById('resultado')
-    var valores = []
-    var numero = Number(numtxt.value)
-    let sel = document.getElementById('selnum')
-
-    if(numero >= 1 && numero <= 100){
-        let item = document.createElement('option')
-        item.text = `valor adiocionado = ${numero}`;
-        sel.appendChild(item)  
-        
+let valor = []
+let num = document.getElementById('numtxt')
+let res = document.getElementById('resultado')
+let tab = document.getElementById('selnum')
+function vernum(n){
+    if(n >= 1 && n <= 100){
+        return true
     }else{
-        alert('[ERRO] valor invalido, pfv verifeque o valor e tente novamente')
+        return false
     }
- 
+}
+function invalor(n, v){
+    if(v.indexOf(n) != -1){
+        return true
+    }else{
+        return false
+    }
 }
 
+function adicionar(){
+    if(vernum(num.value) && !invalor(num.value, valor)){
+        let item = document.createElement('option')
+        item.text = `valor adicionado: ${num.value}`
+        tab.appendChild(item)
+        valor.push(num.value)
+    }else{
+        window.alert('[ERRO] verifique os valores e tente novamente')
+    }
+    num.value = " "
+    num.focus()
+    res.innerHTML = " "
+}
+function finalizar(){
+    if(valor.length == 0){
+        window.alert('preencha os campos vazios')
+    }else{
+        let total = valor.length;
+        let maior = valor[0]
+        let menor = Number(valor[0])
+        let soma = 0
+        let media = 0
+
+        for(let c in valor){
+            soma += Number(valor[c])
+            media = soma / valor.length
+
+            if(Number(valor[c]) > maior){
+                maior = valor[c]
+            }
+            if(Number(valor[c]) < menor){
+                menor = valor[c]
+            }
+            
+        }
+        res.innerHTML = " "
+        res.innerHTML += `<p>temos um total de ${total} numeros cadastrado</p>`
+        res.innerHTML += `<p> o maior valor cadastrado é: ${maior}</p>`
+        res.innerHTML += `<p> o menor valor cadastrado é: ${menor}</p>`
+        res.innerHTML += `<p> a soma dos valores cadastrados é: ${soma}</p>`
+        res.innerHTML += `<p> a media dos valores cadastrados é: ${media}</p>`
+    }
+}
